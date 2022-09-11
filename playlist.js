458fabee4eb7896d68141724a0459caeb306de11
Bluebotlabz <69104218+Bluebotlabz@users.noreply.github.com>
2022-09-11 08:25:56 +0100

    Initial Commit

---
+++ playlist.js
@@ -0,0 +1,24 @@
+//////////////////////////////
+// DO NOT DELETE THIS FILE //
+/////////////////////////////
+
+// Define video playlist as so:
+// videoPlaylist = [
+//    "./Video1.mp4",
+//    "./Video2.mp4"
+// ]
+// 
+// (Don't edit the playlist above, it's just an example)
+// 
+// Make sure the last video in the list doesn't have
+// A comma as the end, but all the others need that
+// Also, they must have ./ at the start and also be
+// Placed in the same folder as this file
+
+
+// Actual playlist Below:
+videoPlaylist = [
+    "./video1.mp4",
+    "./video2.mp4",
+    "./video3.mp4"
+]
